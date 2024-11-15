7aa5d477bbb470914d998cece93cf65b5a9633b2
spbabosm <spbabosm@spbabosmui-MacBookPro.local>
2024-11-15 14:56:36 +0900

    1

---
+++ src/main/frontreact/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Spring Boot + React + NCP</h1>
+      <h3>카운트 : 1</h3>
+      <img src="./image/망상토끼.png" alt="줄여서 망토" />
+    </div>
+  );
+};
+
+export default App;
